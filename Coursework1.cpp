75cfcae663d1f36855adbc0fb640834061c7ebfa
SADAVA <sadava@protonmail.com>
2021-01-06 18:57:50 -0800

    Nightly commit. Working on task 2, improved counting.

--- Coursework1.cpp
+++ Coursework1.cpp
@@ -18,6 +18,9 @@ void PrintTimeStructure(TIME* p);
 void PrintCounter();
 void PrintError(const char* s);
 
+// Task 2
+HEADER_D* InsertItem(HEADER_D* d, int PosStructD = 0, int PosStructA = 0, int Pos2Item = 0, char* pNewItemID = NULL);
+
 // Implementations
 void PrintError(const char* s)
 {
@@ -26,17 +29,20 @@ void PrintError(const char* s)
 
 unsigned int counterDStructures = 0;
 unsigned int counterAStructures = 0;
+unsigned int counter2Items = 0;
 
 void PrintCounter()
 {
     // Prints:
-    // [ 1|2] 
-    // [98|7]
+    // [ 1| 2| 3] 
+    // [98|76|54]
     // etc...
     std::cout << "[";
     std::cout << std::setfill(' ') << std::setw(2) << counterDStructures;
     std::cout << "|";
-    std::cout << std::setfill(' ') << std::setw(1) << counterAStructures;
+    std::cout << std::setfill(' ') << std::setw(2) << counterAStructures;
+    std::cout << "|";
+    std::cout << std::setfill(' ') << std::setw(2) << counter2Items;
     std::cout << "] ";
 }
 
@@ -69,6 +75,8 @@ void PrintItem2Structure(ITEM2* p)
     PrintTimeStructure(pTime);
     std::cout << std::endl;
 
+    counter2Items++;
+
     if (pNext != NULL)
         PrintItem2Structure(pNext);
 }
@@ -76,12 +84,15 @@ void PrintItem2Structure(ITEM2* p)
 void PrintHeaderAStructure(HEADER_A* p)
 {
     if (p == NULL) return PrintError("Can't print Header A structure because it is empty");
-    counterAStructures++;
 
     ITEM2* pItems = (ITEM2*)p->pItems;
+    counter2Items = 0;
     PrintItem2Structure(pItems);
+    counter2Items = 0;
     char cBegin = p->cBegin;
     struct headerA* pNext = p->pNext;
+
+    counterAStructures++;
 
     if (pNext != NULL)
         PrintHeaderAStructure(pNext);
@@ -91,8 +102,6 @@ void PrintHeaderDStructure(HEADER_D* p)
 {
     if (p == NULL) return PrintError("Can't print data structure because it is empty");
 
-    counterDStructures++;
-
     HEADER_A* pHeaderA = p->pHeaderA;
     counterAStructures = 0;
     PrintHeaderAStructure(pHeaderA);
@@ -101,6 +110,8 @@ void PrintHeaderDStructure(HEADER_D* p)
     char cBegin = p->cBegin;
     headerD* pNext = p->pNext;
     headerD* pPrior = p->pPrior;
+
+    counterDStructures++;
 
     if (pNext != NULL)
         PrintHeaderDStructure(pNext);
@@ -113,12 +124,63 @@ void PrintDataStructure(HEADER_D* p)
     counterDStructures = 0;
 }
 
+// Task 2
+HEADER_D* InsertItem(HEADER_D* d, int PosStructD = 0, int PosStructA = 0, int Pos2Item = 0, char* pNewItemID = NULL)
+{
+    // TODO: Ensure ID does not exist
+    // TODO: Ensure id formatting
 
+    for (int i = 0; i < PosStructD; i++)
+    {
+        d = d->pNext;
+
+        if (d == NULL)
+        {
+            PrintError("Reached the limit of D structures");
+            return NULL;
+        }
+    }
+
+    HEADER_A* a = d->pHeaderA;
+
+    for (int i = 0; i < PosStructA; i++)
+    {
+        a = a->pNext;
+
+        if (a == NULL)
+        {
+            PrintError("Reached the limit of A structures");
+            return NULL;
+        }
+    }
+
+    ITEM2* item = (ITEM2*)a->pItems;
+
+    for (int i = 0; i < Pos2Item; i++)
+    {
+        ITEM2* itemNext = a->pNext;
+
+        if (item == NULL)
+        {
+            PrintError("Reached the limit of items");
+            return NULL;
+        }
+
+        a = item;
+    }
+
+    //ITEM2* pNewItem = (ITEM2*)GetItem(2, pNewItemID);
+
+    return d;
+}
 
 int main()
 {
     HEADER_D* p = GetStruct4(2, 20);
     PrintDataStructure(p);
+
+    //HEADER_D* pNew = InsertItem(p, 5, 0, 0, NULL);
+    //PrintDataStructure(p);
 
     return 0;
 }
